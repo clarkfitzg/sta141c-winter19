a230638f9802779f9edc7b6dd1cc077e53e32941
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-01-24 13:25:08 -0800

    finish lecture

--- lecture/outlines/lecture.R
+++ lecture/outlines/lecture.R
@@ -1,128 +1,16 @@
-# Announcements:
 # 
-# - 
-# - My office hours Thursday will be in the Data Science Initiatve, Shields 360, right after class.
+# What we did: dig into this awards data
+# Where we are now: analyzing awards data
+# Where we're going next: Larger data set on the cluster
 # 
-# ## Notes on first HW
+# ## Review
 # 
-# - Grades for HW1 are available.
-#   Ask Siteng in OH if you have questions / clarifications.
-# - Difference between formatting numbers for humans and for machines.
-#   What's this number? 3414992319 is 3.4 billion
-#   Communication- a report is for communicating with humans.
-#   So put them on a table in units that make it easy for humans to read.
-# - Submit homework in a format that Canvas can preview- pdf, html, docx.
-#   We're using the online system to grade- make it easy on the graders.
+# Last lecture I forgot to mention `apply`, that operates over the margins of a matrix, or array more generally.
 # 
 # 
-# on board
+# ## Object sizes in memory
 # 
-# - review independent workers model for parallelism
-# - pictures of replicate, lapply (sapply), tapply, apply, Map, mapply, rapply
-# - What the load balancing cluster apply does with chunking.
-# 
-#  
-# 
-# ## Vectorization
-# 
-# Use `microbenchmark` for small timing experiments.
-# 
-# Vectorized functions operate on entire vectors at once.
-# 
-# ```{r}
-# library(microbenchmark)
-# 
-# n = 1e4
-# x = rnorm(n)
-# 
-# # Fast- Vectorized
-# microbenchmark(y <- exp(x))
-# 
-# # This is not how exp is designed to be used.
-# exp_apply = function(x) sapply(x, exp)
-# 
-# # Slow
-# microbenchmark(y_apply <- exp_apply(x))
-# 
-# exp_loop_preallocated = function(x)
-# {
-#     # Preallocation means we create a place for the answers to go.
-#     result = numeric(length(x))
-#     for(i in seq_along(x)){
-#         result[i] = exp(x[i])
-#     }
-#     result
-# }
-# 
-# # Turn off the byte code compiler
-# enableJIT(0)
-# 
-# # Slow
-# microbenchmark(y_loop_preallocated <- exp_loop_preallocated(y), times = 10L)
-# 
-# 
-# exp_loop_growing = function(x)
-# {
-#     # Dynamically grow the result by appending to the end.
-#     result = c()
-#     for(i in seq_along(x)){
-#         result = c(result, exp(x[i]))
-#     }
-#     result
-# }
-# 
-# # Disaster
-# microbenchmark(y_loop_growing <- exp_loop_growing(y), times = 10L)
-# 
-# ```
-# 
-# Moral of the story?
-# 
-# - Prefer vectorization whenever possible.
-#   String functions such as `tolower, gsub`, etc. are all vectorized.
-# - Apply functions are clean, easier to parallelize, and they always preallocate correctly.
-# - Avoid dynamically growing objects
-# 
-# 
-# 
-# ## Data types
-# 
-# The goal today is to learn a little about how to reason about how large something is.
-# Number crunching is fast because of contiguous blocks of homogeneously typed memory.
-# 
-# Question: What are the possible low level types for R in memory?
-# - 
-# 
-# ```{r}
-# typeof(TRUE)
-# typeof(1L)
-# typeof(1)
-# typeof(1+0i)
-# typeof(raw(10L))
-# ```
-# 
-# Characters and factors are a little different.
-# 
-# ```{r}
-# typeof("words and more words")
-# 
-# f = as.factor(c("female", "male", "female"))
-# 
-# typeof(f)
-# 
-# f
-# ```
-# 
-# Factors are actually stored as integers.
-# How does R know what value to print?
-# 
-# It has an internal lookup table matching the integers to character strings.
-# 
-# ```{r}
-# levels(f)
-# ```
-# 
-# How do we calculate how large an array is?
+# How do we calculate how large a matrix / array is?
 # 
 # ```{r}
 # n = 1000
@@ -141,11 +29,15 @@
 # 
 # ```{r}
 # object.size(x)
+# 
+# object.size(x) - expected_size 
 # ```
 # 
-# Not exactly.
-# Question: Why not?
+# No, the actual object size is _slightly_ larger.
+# Why?
+# 
 # Because R has some memory overhead per object, a few bytes of metadata hanging out for every object.
+# 
 # 
 # Question: How large will it be if we store this in a general container, like a list?
 # 
@@ -156,7 +48,7 @@
 # as.numeric(object.size(xl) / object.size(x))
 # ```
 # 
-# The list is 8 times bigger than the efficient matrix form. :(
+# The list is 8 times bigger than the efficient matrix form.
 # 
 # Which one is faster?
 # 
@@ -167,30 +59,168 @@
 # microbenchmark(do.call(sum, xl))
 # ```
 # 
-# The matrix is 2 orders of magnitude faster.
+# Lesson:
+# Using efficient data structures results in small objects and fast code.
 # 
-# For efficiency we like small objects, and fast code.
+# Which is more efficient?
+# - Millions of objects   :(
+# - Millions of data elements inside one object :)
+# 
+# The latter is far more efficient, both in storage and computation.
 # 
 # 
 # ## Matrix
 # 
-# Use the right data structure to increase speed and reduce memory usage.
+# Everyone loves matrices.
+# Seriously, they're a huge topic in computing.
+# 
+# Matrix package, object oriented programming
+# 
+# Question: From your math classes, what kind of matrices do you know that have special structure?
+# 
+# Classes:
+# - symmetric
+# - diagonal
+# - banded
+# - orthogonal
+# 
+# Question: What kind of methods should we write for matrices?
+# 
+# Methods:
+# - `+, -, %*%`
+# - decompositions
+# 
+# Object oriented programming is about organization and extensibility.
+# 
+# R has _several_ object oriented systems.
+# S3 is the basic one, plot, summary
 # 
 # ```{r}
+# > plot
+# function (x, y, ...)
+# UseMethod("plot")
+# <bytecode: 0x2c790a8>
+# <environment: namespace:graphics>
+# ```
+# 
+# The Matrix package uses the S4 system.
+# Many good references
+# 
+# The [Matrix manual](https://cran.r-project.org/web/packages/Matrix/Matrix.pdf) itself is substantial- 200+ pages. 
+# Explain a little bit what each part does:
+# - Class name
+# - Slots
+# - Extends
+# - Methods
+# - See Also
+# - Examples
+# 
+# The S4 machinery lives in the `methods` package, so we should load that up.
+# 
+# ```{r}
+# library(methods)
+# 
 # library(Matrix)
 # 
-# m = Matrix(1:10, nrow = 2)
+# m = Matrix(1:9, nrow = 3)
 # 
 # class(m)
 # ```
 # 
 # So what's a dgeMatrix?
-# We need to look up the help page for the class.
 # 
 # ```{r}
-# ?dgeMatrix # Nope
-# 
 # class?dgeMatrix
+# ```
+# 
+# A general class for dense matrices and lists many methods that are defined for this class.
+# 
+# ```{r}
+# str(m)
+# ```
+# 
+# This shows the underlying components, called __slots__, that together comprise an object of that class.
+# We can peer into the implementation and access the slots using the `@` operator.
+# 
+# ```{r}
+# m@x
+# m@Dim
+# 
+# class(m@x)
+# class(m@Dim)
+# ```
+# 
+# This shows how the data is actually stored as a one dimensional numeric vector.
+# 
+# There are three ways to create objects of some class:
+# 
+# ```{r}
+# m = Matrix(1:9, nrow = 3)
+# 
+# as(m, "matrix")
+# 
+# new("dgeMatrix", x = rnorm(9), Dim = c(3L, 3L))
+# ```
+# 
+# Question: Will a diagonal matrix be smaller than a dense general matrix?
+# Hopefully, it only has to store n entries.
+# 
+# ```{r}
+# n = 1000
+# x = Matrix(rnorm(n * n), nrow = n)
+# 
+# # More practice: How large should the data in here be?
+# n * n * bytes_per_double
+# 
+# # We're off by ~1KB because of the overhead of having objects in R.
+# object.size(x)
+# ```
+# 
+# Let's construct a diagonal matrix from this.
+# 
+# Question: Can we use `as`?
+# It's a little ambiguous.
+# 
+# ```{r}
+# # fails
+# d = as(x, "ddiMatrix")
+# 
+# # Works, we're being explicit about what we want.
+# d = Diagonal(x = diag(x))
+# ```
+# 
+# Conversely, it's not ambiguous to convert a diagonal matrix into a dense matrix, because there is no loss of information.
+# 
+# ```{r}
+# ddense = as(d, "dgeMatrix")
+# ```
+# 
+# How large should the diagonal matrix be?
+# 
+# ```{r}
+# # Data should be
+# n * bytes_per_double
+# 
+# # But there's some overhead
+# object.size(d)
+# ```
+# 
+# Are diagonal matrices closed under matrix multiplication?
+# Yes.
+# 
+# ```{r}
+# d2 = d %*% d
+# class(d2)
+# ```
+# 
+# Do you think diagonal matrices are faster than dense matrices?
+# 
+# ```{r}
+# library(microbenchmark)
+# 
+# microbenchmark(x %*% x, times = 10L)
+# 
+# microbenchmark(d %*% d, times = 10L)
 # ```
 
 
@@ -200,115 +230,149 @@
 
 
 
+# Object sizes in memory
 
+n = 1000
+p = 500
+x = matrix(rnorm(n * p), nrow = n)
 
+typeof(x)
 
+# Calculate algebraically the size
 
+bytes_per_double = 8
 
+# Size:
+expected = n * p * bytes_per_double
 
+actual = object.size(x)
 
+actual - expected
 
+# Why are the sizes different?
+# Various forms of overhead
 
+# Put it in a list:
+xl = lapply(x, identity)
 
+as.numeric(object.size(xl) / object.size(x) )
+
+# Which one is faster?
 
 library(microbenchmark)
 
-n = 1e4
-x = rnorm(n)
+microbenchmark(sum(x))
 
-# 100 microseconds
-microbenchmark(y <- exp(x))
+microbenchmark(do.call(sum, xl))
 
-# 3 milliseconds
-microbenchmark(y2 <- sapply(x, exp))
+# List is 2 orders of magnitude slower
+# 1 order of magnitude larger than the matrix.
 
-all(y == y2)
+# 2 ways:
+#- Millions of data elements inside one single object :)
+#- Millions of objects :(
 
-exp_loop_preallocate = function(x)
-{
-    # Here's the preallocation:
-    # Make space for our result
-    result = numeric(length(x))
-    for(i in seq_along(x)){
-        result[i] = exp(x[i])
-    }
-}
+# Matrix
 
-library(compiler)
-enableJIT(0)
+# Kinds of special matrices:
+# Classes or all these kinds:
+# - banded
+# - diagonal
+# - identity
+# - orthogonal / orthonormal
+# - invertible
+# - symmetric
+# - zero matrix
+# - sparse (really empty, not dense)
 
-# Without byte code compiler:
-# Around 8 ms
-microbenchmark(y3 <- exp_loop_preallocate(x))
+# banded matrix generalizes a diagonal matrix
+# diagonal generalizes identity
 
-# What if we turn on the byte code compiler?
-enableJIT(0)
+# Operations that we call on matrices:
+# *Methods*
+# - %*%, +, - 
+# - transpose
+# - invert
+# - FFT transform
+# - decompositions, LU, SVD
 
-# Now it's 900 microseconds
-microbenchmark(y3 <- exp_loop_preallocate(x))
+# R has several object oriented systems
 
-# What happens when we do not preallocate?
-exp_loop_bad = function(x)
-{
-    # Dynamically build the result
-    result = c()
-    for(i in seq_along(x)){
-        result = c(result, exp(x[i]))
-    }
-}
+# S3 is very common
+methods(plot)
 
-# Around 160 milliseconds
-microbenchmark(y4 <- exp_loop_bad(x), times = 10L)
+# S4 adds rigor, more predictability, multiple dispatch
 
-# vectorized: 100 microseconds
-# sapply: 4 ms
-# with preallocation: 8 ms
-# without preallocation: 160 ms
+library(methods)
 
-# Takeaways:
-# - Always use vectorization if you can.
-# Why do we like apply?
-# - easier code to write and read
-# - it's easy to make it parallel
-# - not possible to make "growing vector" error
+library(Matrix)
 
+m = Matrix(1:9, nrow = 3)
 
-# Data types
+class?dgeMatrix
 
-# What are the possible data types in R?
+str(m)
 
-# How it acts
-class(1)
+# Called a 'slot' in S4 terms
+m@x
 
-# How it is stored / represented in memory
-typeof(1)
+m@Dim
 
-typeof(TRUE)
+# Three ways to make a new object of some class:
 
-typeof(1L)
+# Constructor
+m = Matrix(1:9, nrow = 3)
 
-typeof(1)
+# Coercion as() 
+as(m, "matrix")
 
-r = as.raw(1:4)
+as(m, "numeric")
 
-typeof(r)
+# Explicitly calling new()
 
-d = as.Date("2018-10-10")
-typeof(d)
-class(d)
+new("dgeMatrix", x = as.numeric(1:9), Dim = c(3L, 3L))
 
-f = as.factor(c("male", "female", "female"))
+new("dgeMatrix", x = 1:9, Dim = c(3L, 3L))
 
-class(f)
+# Question:
+# Will a diagonal matrix be smaller than a dense general matrix?
 
-typeof(f)
+n = 1000
+x = Matrix(rnorm(n * n), nrow = n)
 
-as.integer(f)
+# How large should x be?
+n * n * bytes_per_double
 
-levels(f)
+object.size(x)
 
+# The class is ddiMatrix
+d = as(x, "ddiMatrix")
 
+diag(x)
 
-typeof("abc")
+d = Diagonal(x = diag(x))
 
+dim(d)
+
+# How big should this diagonal matrix be?
+# (In theory)
+n * bytes_per_double
+
+object.size(d)
+
+ddense = as(d, "dgeMatrix")
+
+object.size(ddense)
+
+# Are diagonal matrices closed under multiplication?
+d2 = d %*% d
+
+class(d2)
+
+# Are diagonal matrices faster than dense matrices?
+
+microbenchmark(ddense %*% ddense, times = 10L)
+
+microbenchmark(d %*% d, times = 10L)
+# 4 orders of magnitude faster
 
