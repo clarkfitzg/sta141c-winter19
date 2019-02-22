b94521ae5a91b5e8af6ef3b8cc2c5b57bbbd7970
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-02-22 15:23:19 -0800

    profiling experiment

--- examples/profile.sh
+++ examples/profile.sh
@@ -1,8 +1,14 @@
 # Profiling our python script
 
 DATAFILE=/Users/clark/data/transaction.zip
+ONLY_COLUMNS=amt_agency.csv
+FIRST_DIGIT=first_digits.csv
 AGENCY=18
 AMT=8
 
-cut --delimiter=, --fields=${AGENCY},${AMT} ${DATAFILE} |
-    head -n 100000 > amt_agency.csv
+unzip -p ${DATAFILE}  |
+    cut -d , -f ${AGENCY},${AMT} |
+    head -n 100000 > ${ONLY_COLUMNS}
+
+# Interesting
+python3 -m cProfile -s tottime first_char_first_col.py ${ONLY_COLUMNS} ${FIRST_DIGIT}
