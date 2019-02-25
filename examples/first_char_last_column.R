5b26c7905900e467943c4dd92878bf0fe3442715
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-02-25 11:25:08 -0800

    not working now

--- examples/first_char_last_column.R
+++ examples/first_char_last_column.R
@@ -1,26 +1,29 @@
-
-
-# Suppose I wanted to pass arbitrary arguments into read.table. How? ...
-read_chunk = function(file, sep = ",", nrows = 1e5L)
-{
-    read.table(file, sep = sep, nrows = nrows)
-}
-
+# For the last column, print only the first character.
+# Usage:
+# 
+#     $ echo "100,200" | Rscript first_char_last_column.R
+# 
+# Should print "100,2"
 
 input = file("stdin")
+open(input)
 
 output = stdout()
 
 
-chunk = read_chunk(input)
-lastcol = ncol(chunk)
+while(isIncomplete(input)){
+# If this gets too complicated we could pull each step out into it's own function
 
-# If this got complicated we could pull the process and write steps out into their own functions, like the read one.
+    # 1. Read
+    chunk = read.table(file, sep = ",", nrows = 1e5L)
 
-while(0 < nrow(chunk)){
+    # 2. Process
+    lastcol = ncol(chunk)
     # Replace the last column of strings with the first character
     chunk[, lastcol] = substring(chunk[, lastcol], 1L, 1L)
-    write.table(chunk, output, sep = ",")
+
+    # 3. Write
+    write.table(chunk, output, sep = ",", quote = FALSE, col.names = FALSE, row.names = FALSE)
 }
 
 # Idea: demonstrate map reduce on a table.
