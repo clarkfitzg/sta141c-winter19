13924deefce989c0a62529a041f80c412d191a98
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-02-28 17:01:27 -0800

    examples from profiling in class

--- examples/profile.sh
+++ examples/profile.sh
@@ -1,6 +1,6 @@
 # Profiling our python script
 
-DATAFILE=/Users/clark/data/transaction.zip
+DATAFILE=/scratch/transaction.zip
 ONLY_COLUMNS=amt_agency.csv
 FIRST_DIGIT=first_digits.csv
 AGENCY=18
@@ -16,5 +16,10 @@ python3 -m cProfile -s tottime first_char_first_col.py ${ONLY_COLUMNS} ${FIRST_D
 
 unzip -p ${DATAFILE}  |
     cut -d , -f ${AGENCY},${AMT} |
-    python3 -m cProfile -o pyprof.out first_char_last_column.py |
-    Rscript first_char_last_column.R > test.txt
+    python3 -m cProfile -o pyprof.out first_char_last_column.py > test.txt
+    #Rscript first_char_last_column.R > test.txt
+    
+# Don't unzip, just csv file
+cut -d , -f ${AGENCY},${AMT} /scratch/transaction.csv |
+    python3 -m cProfile -o pyprof.out first_char_last_column.py > test.txt
+    #Rscript first_char_last_column.R > test.txt
